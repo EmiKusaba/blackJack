0134ef9c4ecb502a6c3096369b55d5e60f374b4f
EmiKusaba <eeemi615@gmail.com>
2022-01-06 15:13:34 -0600

    added new file

---
+++ selfTry1.js
@@ -0,0 +1,32 @@
+//cards
+class Card {
+  constructor(suit, number) {
+    this.suit = suit
+    this.number = number
+  }
+}
+
+//users
+
+class User {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+//game
+class Game {
+  constructor() {
+    this.users = []
+    for (let i = 0; i < 2; i++) {
+      this.users.push(new User(i))
+  }
+    this.cards = []
+    const suits = ["hearts", "diamonds", "spades", "clubs"]
+    for(let suit in suits)
+    const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    for(let number in numbers){
+      this.cards.push(new Card(suit, number))
+    }
+  }
+}
