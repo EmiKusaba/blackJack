c428dcafedfb6c9147531ab556a0fb1d430b32fa
EmiKusaba <eeemi615@gmail.com>
2022-01-11 14:43:05 -0600

    added game func

--- selfTry3.js
+++ selfTry3.js
@@ -18,16 +18,38 @@ class User {
 
 class Game {
   constructor() {
-   this.users = []
-   for(let i = 0; i < 2; i++) {
-    this.users.push(new User[i])
-   }
-   let suits = ["hearts", "diamonds", "spades", "clubs"]
-   for(suits in suit) {
-     
-   }
-   let number = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-   let deck = []
+    this.users = []
+    for (let i = 0; i < 2; i++) {
+      this.users.push(new User(i))
+    }
+
+    this.deck = []
+    const suits = ["hearts", "diamonds", "spades", "clubs"]
+    for (let suit in suits) {
+      const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+      for(let number in numbers) {
+        this.deck.push(new Card(suit, number))
+      }
+    }
+  }
+
+  shuffle(){
+    const getRandomInt = (max) => {
+      return Math.floor(Math.random() * max)
+    }
+    let deck = []
+    for(let i = 0; i < this.deck.length; i++){
+    let index = getRandomInt(this.deck.length)
+    let card = this.deck[index]
+
+    }
+  }
+
+  deal(user){
+
+  }
+
+  play() {
 
   }
 }
