78ee0f27b38033783659f44bb3bb7750556f2777
EmiKusaba <eeemi615@gmail.com>
2022-01-14 15:10:49 -0600

    added selfTry7 file

---
+++ selfTry7.js
@@ -0,0 +1,51 @@
+//card
+class Card {
+  constructor(suit, number) {
+    this.suit = suit
+    this.number = number
+  }
+}
+
+//user
+class User {
+  constructor(name, card) {
+    this.name = name
+    this.card = card
+  }
+}
+//game
+
+class Game {
+  constructor() {
+    this.users = []
+    for (let i = 0; i < 2; i++) {
+      this.users.push(new User(i))
+    }
+    this.deck = []
+    let suits = [""]
+    for (let suit in suits) {
+      let numbers = [""]
+      for (let number in numbers) {
+        this.deck.push(new Card(suit, number))
+      }
+    }
+  }
+  //suffle
+  suffle() {
+    const getRandomInt = (max) => {
+      return Math.floor(Math.random() * max);
+    }
+    
+  }
+
+
+  //deal
+  deal(user) {
+
+  }
+
+  //play
+  play() {
+
+  }
+}
