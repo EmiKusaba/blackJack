dc14ca896719c29ac99bb95e0920f066102a6d4a
EmiKusaba <eeemi615@gmail.com>
2022-01-17 16:01:58 -0600

    added choice and display function

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 //card class
 
 class Card {
   constructor(suit, number) {
     this.suit = suit
     this.number = number
+  }
+
+  name() {
+    return `${this.number} of ${this.suit}`
   }
 }
 
@@ -26,10 +36,10 @@ class Game {
 
     this.deck = []
     const suits = ["hearts", "diamonds", "spades", "clubs"]
-    for (let suit in suits) {
+    for (let i = 0; i < suits.length; i++) {
       const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-      for (let number in numbers) {
-        this.deck.push(new Card(suit, number))
+      for (let j = 0; j < numbers.length; j++) {
+        this.deck.push(new Card(suits[i], numbers[j]))
       }
     }
   }
@@ -57,10 +67,41 @@ class Game {
 
   play() {
     this.shuffle()
-    for(let i = 0; i < 2; i++) {
-      for(let j = 0; j < this.users.length; j++) {
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < this.users.length; j++) {
         this.deal(this.users[j])
+      }
+    }
+
+    this.display()
+
+    for(let i = 0; i < this.users.length; i++) {
+      this.choice(this.users[i])
+    }
+
+    this.display()
+  }
+
+  choice(user) {
+    rl.question(`User ${user.name} hit? [y/n]`, (ans) => {
+      if ("y" === ans) {
+        this.deal(user)
+      }
+    })
+  }
+
+  display() {
+    for (let i = 0; i < this.users.length; i++) {
+      let user = this.users[i]
+      console.log(`User ${user.name}'s cards:`)
+      for (let j = 0; j < user.cards.length; j++) {
+        console.log(user.cards[j].name())
       }
     }
   }
 }
+
+// Main
+
+let game = new Game()
+game.play()
