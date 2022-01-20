9e08c70bb3c99fab89730a9d138dbf549ab1d417
EmiKusaba <eeemi615@gmail.com>
2022-01-20 14:43:01 -0600

    added suffle and deal func at selfTry1

--- selfTry1.js
+++ selfTry1.js
@@ -9,8 +9,9 @@ class Card {
 //users
 
 class User {
-  constructor(name) {
+  constructor(name, card) {
     this.name = name
+    this.cards = []
   }
 }
 
@@ -20,13 +21,34 @@ class Game {
     this.users = []
     for (let i = 0; i < 2; i++) {
       this.users.push(new User(i))
-  }
+    }
     this.cards = []
     const suits = ["hearts", "diamonds", "spades", "clubs"]
-    for(let suit in suits)
-    const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-    for(let number in numbers){
+    for (let suit in suits)
+      const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    for (let number in numbers) {
       this.cards.push(new Card(suit, number))
     }
+  }
+  suffle() {
+    const getRandomInt = (max) => {
+      return Math.floor(Math.random() * max);
+    }
+    let deck = []
+    for (let i = 0; i < this.deck.length; i++) {
+      let index = getRandomInt(this.deck.length)
+      let card = this.deck[index]
+      deck.push(card)
+      this.deck.splice(index, 1)
+    }
+    this.deck = deck
+  }
+  deal(user) {
+    let card = this.deck.pop()
+    user.cards.push(card)
+  }
+
+  play() {
+
   }
 }
