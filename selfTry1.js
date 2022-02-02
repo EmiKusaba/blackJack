afbbc10e5c3ec5b4f2a4c3239c02785b806c41f2
EmiKusaba <eeemi615@gmail.com>
2022-02-02 15:15:17 -0600

    added all details

--- selfTry1.js
+++ selfTry1.js
@@ -33,15 +33,41 @@ class User {
 
   score() {
     let sums = [0]
-    for(let i = 0; i < this.cards.length; i++) {
-      let card = this.cards[i]
+
+    for (let i = 0; i < this.cards.length; i++) {
+      const card = this.cards[i]
+
       if (card.number === "Ace") {
+        sums = sums.concat(sums)
+        for (let j = 0; j < sums.length; j++) {
+          if (j < sums.length / 2) {
+            sums[j] += card.score(false)
+          } else {
+            sums[j] += card.score(true)
+          }
+        }
+      } else {
+        for (let j = 0; j < sums.length; j++) {
+          sums[j] += card.score()
+        }
       }
     }
 
-  }
+    sums.sort((a, b) => {
+      if (a > 21) {
+        return 1
+      } else if (b > 21) {
+        return -1
+      }
 
+      return b - a
+    })
+
+    return sums[0]
+  }
 }
+
+
 
 //game class
 class Game {
@@ -86,7 +112,7 @@ class Game {
 
     this.shuffle()
 
-    for(let i = 0; i < 2; i++) {
+    for (let i = 0; i < 2; i++) {
       for (let j = 0; j < this.users.length; j++) {
         this.deal(this.users[j])
       }
@@ -100,13 +126,13 @@ class Game {
 
     this.win()
   }
-  
+
   display() {
     console.log()
-    for(let i = 0; i < this.users.length; i++) {
+    for (let i = 0; i < this.users.length; i++) {
       let user = this.users[i]
       console.log(`User ${user.name}'s cards:`)
-      for(let j = 0; j < user.cards.length; j++) {
+      for (let j = 0; j < user.cards.length; j++) {
         console.log(user.cards[j].name())
       }
       console.log(`score: ${user.score()}`)
@@ -114,7 +140,43 @@ class Game {
       console.log()
     }
   }
-  choice() {
+  choice(user) {
+    return new Promise((resolve) => {
+      rl.question(`User ${user.name} hit? [y/n]\n`, (ans) => {
+        if ("y" === ans) {
+          this.deal(user)
+        }
+        resolve()
+      })
+    })
+  }
+  win() {
+    let scores = []
 
+    for (let i = 0; i < this.users.length; i++) {
+      let user = this.users[i]
+      let score = user.score()
+      scores.push([score, user])
+    }
+
+    scores.sort((a, b) => {
+      if (a[0] > 21) {
+        return 1
+      } else if (b[0] > 21) {
+        return -1
+      }
+      return b[0] - a[0]
+    })
+
+    for (let i = 0; i < scores.length; i++) {
+      const score = scores[i][0]
+      const user = scores[i][1]
+      if (score !== scores[0][0]) {
+        break
+      }
+      console.log(`User ${user.name} wins!`)
+    }
   }
 }
+let game = new Game()
+game.play()
