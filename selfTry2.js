fe1672dbb416a042de786bdf631bee7d00316580
EmiKusaba <eeemi615@gmail.com>
2022-02-03 21:46:59 -0600

    added feature

--- selfTry2.js
+++ selfTry2.js
@@ -3,3 +3,40 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+//user
+class User {
+  constructor(name) {
+    this.name = name
+    this.cards = []
+  }
+  score() {
+
+  }
+}
+
+//card
+class Card {
+  constructor(number, suit) {
+    this.number = number
+    this.suit = suit
+  }
+
+}
+
+//game
+class Game {
+  constructor() {
+    this.users = []
+    
+  }
+  shuffle() {
+
+  }
+  deal() {
+
+  }
+  play() {
+
+  }
+}
