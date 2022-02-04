5d3f330f4c2ffefbc31cf93966d3e7c9d0d709eb
EmiKusaba <eeemi615@gmail.com>
2022-02-04 15:27:16 -0600

    added this deck at game class

--- selfTry2.js
+++ selfTry2.js
@@ -11,7 +11,13 @@ class User {
     this.cards = []
   }
   score() {
+    let sums = [0]
+    for (let i = 0; i < this.cards.length; i++) {
+      let card = this.card[i]
+      if (card === "Ace") {
 
+      }
+    }
   }
 }
 
@@ -21,19 +27,41 @@ class Card {
     this.number = number
     this.suit = suit
   }
+  name() {
+    return `${this.number} of ${this.suit}`
+  }
 
+  score(aceIs11) {
+    if (this.number === "Ace") {
+      return aceIs11 ? 11 : 1
+    }
+    if (this.number === "K" || this.number === "Q" || this.number === "J") {
+      return 10
+    }
+    return Number(this.number)
+  }
 }
 
 //game
 class Game {
   constructor() {
     this.users = []
-    
+    for (let i = 0; i < 2; i++) {
+      this.users.push(new User(i))
+    }
+    this.deck = []
+    const suits = ["hearts", "diamonds", "spades", "clubs"]
+    for (let i = 0; i < suits.length; i++) {
+      const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+      for (let j = 0; j < numbers.length; j++) {
+        this.desk.push(new Card(suits[i], numbers[j]))
+      }
+    }
   }
   shuffle() {
-
+    
   }
-  deal() {
+  deal(user) {
 
   }
   play() {
