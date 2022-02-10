911d920d70b6c95bf5d526ddba23149bd922cf27
EmiKusaba <eeemi615@gmail.com>
2022-02-10 15:30:19 -0600

    added shuffle func at selfTry3

--- selfTry3.js
+++ selfTry3.js
@@ -5,39 +5,109 @@ const rl = readline.createInterface({
 });
 //card
 class Card {
-  constructor(number, suit){
-  this.number = number
-  this.suit = suit
+  constructor(number, suit) {
+    this.number = number
+    this.suit = suit
+  }
+  score(aceIs11) {
+    if(this.number === "Ace") {
+      return aceIs11 ? 11 : 1
+    }
+    if(this.number === "K" || this.number === "Q" || this.number === "J") {
+      return 10
+    }
+    return Number(this.number)
   }
 }
 
 //user
 class User {
   constructor(name) {
-    this.name  = name
+    this.name = name
     this.card = []
   }
+  
   score() {
+    let sums = [0]
 
+    for (let i = 0; i < this.cards.length; i++) {
+      const card = this.cards[i]
+
+      if (card.number === "Ace") {
+        sums = sums.concat(sums)
+        for (let j = 0; j < sums.length; j++) {
+          if (j < sums.length / 2) {
+            sums[j] += card.score(false)
+          } else {
+            sums[j] += card.score(true)
+          }
+        }
+      } else {
+        for (let j = 0; j < sums.length; j++) {
+          sums[j] += card.score()
+        }
+      }
+    }
+
+    sums.sort((a, b) => {
+      if (a > 21) {
+        return 1
+      } else if (b > 21) {
+        return -1
+      }
+
+      return b - a
+    })
+
+    return sums[0]
   }
 }
 
 //game
 
 class Game {
-  constructor() {
-    this.users = []
-    for(let i = 0; i < 2; i++){
-      this.users.push(new User[i])
+    constructor() {
+      this.users = []
+      for (let i = 0; i < 2; i++) {
+        this.users.push(new User(i))
+      }
+  
+      this.deck = []
+      const suits = ["hearts", "diamonds", "spades", "clubs"]
+      for (let i = 0; i < suits.length; i++) {
+        const numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        for (let j = 0; j < numbers.length; j++) {
+          this.deck.push(new Card(suits[i], numbers[i]))
+        }
+      }
+    }
+    shuffle() {
+      let getRandomInt = (max) => {
+        return Math.floor(Math.random() * max);
+      }
+      let deck = []
+      let len = this.deck.length
+      for(let i = 0; i < len.length; i++) {
+        let index = getRandomInt(this.deck.length)
+        let card = this.deck.splice(index, 1)[0]
+      deck.push(card)
+    }
+    this.deck = deck
+      
+    }
+    deal() {
+
+    }
+    play() {
+
+    }
+    choice() {
+
+    }
+    display() {
+
+    }
+    win() {
+
     }
   }
-  shuffle(){
-
-  }
-  deal(){
-
-  }
-  play() {
-
-  }
-}
