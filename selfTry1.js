5b98ea7b1fac2727ffe00d0c3ee331daecebf176
EmiKusaba <eeemi615@gmail.com>
2022-02-22 15:44:37 -0600

    added game func

--- selfTry1.js
+++ selfTry1.js
@@ -4,65 +4,86 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
 //card class
 
 class Card {
-  constructor(number, suit) {
-    this.number = number
+  constructor(suit, number) {
     this.suit = suit
+    this.number = number
   }
 
   name() {
     return `${this.number} of ${this.suit}`
   }
+
   score(aceIs11) {
-    if(this.number === "Ace") {
+    if (this.number === "Ace") {
       return aceIs11 ? 11 : 1
     }
-    if(this.number === "K" || this.number === "Q" || this.number === "J") {
+
+    if (this.number === "J" || this.number === "Q" || this.number === "K") {
       return 10
     }
+
     return Number(this.number)
   }
 }
 
-//user class
+//User
 
 class User {
-  constructor(name, cards) {
+  constructor(name) {
     this.name = name
     this.cards = []
   }
-  score(){
+  score() {
     let sums = [0]
-    for(let i = 0; i < this.cards.length; i++) {
+
+    for (let i = 0; i < this.cards.length; i++) {
       const card = this.cards[i]
-        if (card.number === "Ace") {
-          sums = sums.concat(sums)
-          for (let j = 0; j < sums.length; j++) {
-            if (j < sums.length / 2) {
-              sums[j] += card.score(false)
-            } else {
-              sums[j] += card.score(true)
-            }
-          }
-        } else {
-          for (let j = 0; j < sums.length; j++) {
-            sums[j] += card.score()
+
+      if (card.number === "Ace") {
+        sums = sums.concat(sums)
+        for (let j = 0; j < sums.length; j++) {
+          if (j < sums.length / 2) {
+            sums[j] += card.score(false)
+          } else {
+            sums[j] += card.score(true)
           }
         }
-}
-
-//game class
-
-class Game {
-  constructor(users) {
-    this.users = []
-    for(let i = 0; i < 2; i++){
-      this.users.push(new User(i))
+      } else {
+        for (let j = 0; j < sums.length; j++) {
+          sums[j] += card.score()
+        }
+      }
     }
 
+    sums.sort((a, b) => {
+      if (a > 21) {
+        return 1
+      } else if (b > 21) {
+        return -1
+      }
+
+      return b - a
+    })
+
+    return sums[0]
+  }
+}
+//Game
+
+class Game {
+  constructor(){
+    this.users = []
+    for(let i = 0; i < 2; i++){
+    this.users.push(new User(i))
+    }
+  }
+
+  shuffle(){
 
   }
+  
+  
 }
