8754a3a0cc90c17b4ca9792a34de84bf5c873426
EmiKusaba <eeemi615@gmail.com>
2022-03-09 15:00:44 -0600

    added display, choice function at selfTry2

--- selfTry2.js
+++ selfTry2.js
@@ -6,11 +6,11 @@ const rl = readline.createInterface({
 
 //user
 class User {
-  constructor(name, cards){
+  constructor(name, cards) {
     this.name = name
     this.cards = []
   }
-  score(){
+  score() {
 
   }
 }
@@ -18,19 +18,19 @@ class User {
 // card
 
 class Card {
-  constructor(number, suits){
+  constructor(number, suits) {
     this.number = number
     this.suits = suits
   }
-  name(){
+  name() {
     return `${this.number}of${this.suits}`
   }
 
-  score(aceI11){
-    if(this.number === "Ace") {
+  score(aceI11) {
+    if (this.number === "Ace") {
       return aceIs11 ? 11 : 1
     }
-    if(this.number === "K" || this.number === "Q" || this.number === "J"){
+    if (this.number === "K" || this.number === "Q" || this.number === "J") {
       return 10
     }
     return new Number(this.number)
@@ -42,14 +42,14 @@ class Card {
 class Game {
   constructor() {
     this.users = []
-    for(let i = 0; i < 2; i++) {
+    for (let i = 0; i < 2; i++) {
       this.users.push(new User(i))
     }
     this.deck = []
     let suits = ["hearts", "diamonds", "spades", "clubs"]
-    for(let i = 0; i < suits.length; i++) {
+    for (let i = 0; i < suits.length; i++) {
       let numbers = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-      for(let j = 0; j < numbers.length; j++) {
+      for (let j = 0; j < numbers.length; j++) {
         this.deck.push(new Card(suits[i], numbers[j]))
       }
     }
@@ -60,7 +60,7 @@ class Game {
     let getRandomInt = (max) => {
       return Math.floor(Math.random() * max);
     }
-    for(let i = 0; i < len; i++) {
+    for (let i = 0; i < len; i++) {
       let index = getRandomInt(this.deck.length)
       let card = this.deck.splice(index, 1)[0]
       deck.push(card)
@@ -76,28 +76,58 @@ class Game {
   display() {
     console.log()
 
-    for(let i = 0; i < this.users.length; i ++) {
+    for (let i = 0; i < this.users.length; i++) {
       let user = this.users[i]
       console.log(`User ${user.name}'s Cards'`)
-      for(let j = 0; j < user.cards.length; j++) {
+      for (let j = 0; j < user.cards.length; j++) {
         console.log(`${user.cards[j].name()}`)
       }
       console.log(`score: ${user.score()}`)
-      
+
       console.log()
 
     }
   }
 
   choice() {
+    return new Promise((resolve) => {
+      rl.question(`User ${user.name} hit? [y/n]\n`, (ans) => {
+        if ("y" === ans) {
+          this.deal(user)
+        }
+        resolve()
+      })
+    })
 
   }
 
   win() {
+    
 
   }
 
   play() {
 
+    this.shuffle()
+
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < this.users.length; i++) {
+        this.deal(this.users[j])
+      }
+    }
+
+    this.display()
+
+    for (let i = 0; i < this.users.length; i++) {
+      await this.choice(this.users[i])
+    }
+
+    this.display()
+
+    this.win()
+
   }
 }
+
+let game = new Game
+game.play()
